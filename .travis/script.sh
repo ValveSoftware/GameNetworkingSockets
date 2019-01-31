40263e3f8338e20fe1571808955640cbb9ec43ed
Steven Noonan <steven@valvesoftware.com>
2019-01-30 21:05:38 -0800

    travis/script: enable -Werror for builds
    This may get obnoxious, but we may be able to maintain completely
    warning-free code.
    
    Signed-off-by: Steven Noonan <steven@valvesoftware.com>

--- .travis/script.sh
+++ .travis/script.sh
@@ -8,27 +8,38 @@ set -ex
 
 rm -rf build-{a,ub,t}san build-meson build-cmake
 
+CMAKE_ARGS=(
+	-G Ninja
+	-DLIGHT_TESTS:BOOL=ON
+	-DWERROR=ON
+)
+
+MESON_ARGS=(
+	-Dlight_tests=true
+	-DWerror=true
+)
+
 BUILD_SANITIZERS=1
 [[ $(uname -s) == MINGW* ]] && BUILD_SANITIZERS=0
 
 # Build some tests with sanitizers
 if [[ $BUILD_SANITIZERS -ne 0 ]]; then
 	mkdir build-asan
-	(cd build-asan && cmake -G Ninja -DSANITIZE_ADDRESS:BOOL=ON -DLIGHT_TESTS:BOOL=ON ..)
+	(cd build-asan && cmake ${CMAKE_ARGS[@]} -DSANITIZE_ADDRESS:BOOL=ON ..)
 
 	mkdir build-ubsan
-	(cd build-ubsan && cmake -G Ninja -DSANITIZE_UNDEFINED:BOOL=ON -DLIGHT_TESTS:BOOL=ON ..)
+	(cd build-ubsan && cmake ${CMAKE_ARGS[@]} -DSANITIZE_UNDEFINED:BOOL=ON ..)
 
 	if [[ ${CXX} == *clang* ]]; then
 		mkdir build-tsan
-		(cd build-tsan && cmake -G Ninja -DSANITIZE_THREAD:BOOL=ON -DLIGHT_TESTS:BOOL=ON ..)
+		(cd build-tsan && cmake ${CMAKE_ARGS[@]} -DSANITIZE_THREAD:BOOL=ON ..)
 	fi
 fi
 
 # Build lightweight test builds
 mkdir build-cmake
-(cd build-cmake && cmake -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLIGHT_TESTS:BOOL=ON ..)
-meson . build-meson -Dlight_tests=true --buildtype debugoptimized
+(cd build-cmake && cmake ${CMAKE_ARGS[@]} -DCMAKE_BUILD_TYPE=RelWithDebInfo ..)
+meson . build-meson ${MESON_ARGS[@]} --buildtype debugoptimized
 
 # Build all targets of CMake/meson, ensuring everything can build.
 ninja -C build-meson
