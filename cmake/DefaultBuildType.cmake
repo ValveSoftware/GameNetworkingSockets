b5db1d1a0a512844a6afc33e2479522bece58ddf
Steven Noonan <steven@valvesoftware.com>
2019-03-01 12:56:21 -0800

    cmake: set RelWithDebInfo as default build type
    Signed-off-by: Steven Noonan <steven@valvesoftware.com>

---
+++ cmake/DefaultBuildType.cmake
@@ -0,0 +1,10 @@
+# Set a default build type if none was specified
+set(default_build_type "RelWithDebInfo")
+
+if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+  message(STATUS "Setting build type to '${default_build_type}' as none was specified.")
+  set(CMAKE_BUILD_TYPE "${default_build_type}" CACHE STRING "Choose the type of build." FORCE)
+
+  # Set the possible values of build type for cmake-gui
+  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
+endif()
