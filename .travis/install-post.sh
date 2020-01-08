43692c9599bc00e514f315b345cec434cf03728f
Steven Noonan <steven@valvesoftware.com>
2020-01-08 00:52:54 -0800

    travis/install-post: dump GCC and Clang predefined macros
    Useful for debugging cross-platform build issues.
    
    Signed-off-by: Steven Noonan <steven@valvesoftware.com>

--- .travis/install-post.sh
+++ .travis/install-post.sh
@@ -9,4 +9,14 @@ g++ --version
 clang++ --version
 uname -a
 
+echo "====================="
+echo "GCC predefined macros"
+echo "====================="
+g++ -E -dM -xc /dev/null | sort
+
+echo "======================="
+echo "Clang predefined macros"
+echo "======================="
+clang++ -E -dM -xc /dev/null | sort
+
 exit 0
