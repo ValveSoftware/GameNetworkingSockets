b187cc12d4d66b2fe9d6d712a1b313678dec95f0
Steven Noonan <steven@valvesoftware.com>
2020-01-07 20:58:48 -0800

    travis: don't partition tests by compiler
    This may end up reducing build time, because we won't have to wait for
    multiple different Travis VMs to spawn for each compiler. We will also
    be using a much smaller VM, since we aren't actually using the GCC/Clang
    available in the system that's just going to end up running all our
    tests via Docker anyway.
    
    Signed-off-by: Steven Noonan <steven@valvesoftware.com>

--- .travis/build.sh
+++ .travis/build.sh
@@ -13,7 +13,28 @@ cleanup() {
 trap cleanup EXIT
 cleanup
 
-bash .travis/build-meson.sh
-bash .travis/build-cmake.sh
+FOUND_CLANG=0
+FOUND_GCC=0
+
+if type -P clang && type -P clang++; then
+	echo "Beginning build tests with Clang" >&2
+	export CC=clang CXX=clang++
+	bash .travis/build-meson.sh
+	bash .travis/build-cmake.sh
+	FOUND_CLANG=1
+fi
+
+if type -P gcc && type -P g++; then
+	echo "Beginning build tests with GCC" >&2
+	export CC=gcc CXX=g++
+	bash .travis/build-meson.sh
+	bash .travis/build-cmake.sh
+	FOUND_GCC=1
+fi
+
+if [[ $FOUND_CLANG -eq 0 ]] && [[ $FOUND_GCC -eq 0 ]]; then
+	echo "FAILED: Couldn't find either Clang or GCC, no tests were run." >&2
+	exit 1
+fi
 
 exit 0
