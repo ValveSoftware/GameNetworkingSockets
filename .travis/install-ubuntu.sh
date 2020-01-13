0ce1fbd4fff1d8278d63c19096c7bc01c92903bb
sidhujag <jsidhu@blockchainfoundry.co>
2020-01-13 09:08:35 -0800

    fix ci

--- .travis/install-ubuntu.sh
+++ .travis/install-ubuntu.sh
@@ -8,14 +8,10 @@ apt-get update
 apt-get install -y locales
 locale-gen en_US.UTF-8
 
-PACKAGES=(build-essential pkg-config cmake meson clang)
+PACKAGES=(build-essential pkg-config cmake meson clang libjsonrpccpp-dev libjsonrpccpp-tools)
 
 PACKAGES+=(libprotobuf-dev protobuf-compiler)
 PACKAGES+=(libssl-dev)
-PACKAGES+=(libcurl4-openssl-dev)
-PACKAGES+=(libjsoncpp-dev)
-PACKAGES+=(libjsonrpccpp-dev)
-PACKAGES+=(libjsonrpccpp-tools)
 
 apt-get install -y "${PACKAGES[@]}"
 
