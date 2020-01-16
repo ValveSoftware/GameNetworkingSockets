3d67aefe4bb4c5a9684e67de1e989787bf8ff757
sidhujag <jsidhu@blockchainfoundry.co>
2020-01-16 07:43:31 -0800

    Revert "compile"
    This reverts commit 93c1558c3a4b3d5fd83a6e528ffc255fa3cc7170.

--- gateway/gateway.cpp
+++ gateway/gateway.cpp
@@ -100,7 +100,7 @@ static void Printf( const char *fmt, ... )
 	DebugOutput( k_ESteamNetworkingSocketsDebugOutputType_Msg, text );
 }
 
-STEAMNETWORKINGSOCKETS_INTERFACE ISteamNetworkingSockets* InitSteamDatagramConnectionSockets()
+static CSteamNetworkingSockets* InitSteamDatagramConnectionSockets()
 {
 	SteamDatagramErrMsg errMsg;
 	CSteamNetworkingSockets *pSteamNetworkingSockets = new CSteamNetworkingSockets( ( CSteamNetworkingUtils *)SteamNetworkingUtils() );
@@ -116,7 +116,7 @@ STEAMNETWORKINGSOCKETS_INTERFACE ISteamNetworkingSockets* InitSteamDatagramConne
 	return pSteamNetworkingSockets;
 }
 
-static void ShutdownSteamDatagramConnectionSockets(ISteamNetworkingSockets* pSteamNetworkingSockets)
+static void ShutdownSteamDatagramConnectionSockets(CSteamNetworkingSockets* pSteamNetworkingSockets)
 {
 	// Give connections time to finish up.  This is an application layer protocol
 	// here, it's not TCP.  Note that if you have an application and you need to be
