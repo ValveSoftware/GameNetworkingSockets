943c26c32b89f599d6576b3a6bcb0ae347b863de
Steven Noonan <steven@valvesoftware.com>
2020-01-21 02:43:20 -0800

    ed25519/curve25519: only build SSE2 variants on x86
    Signed-off-by: Steven Noonan <steven@valvesoftware.com>

--- src/external/curve25519-donna/curve25519_VALVE_sse2.c
+++ src/external/curve25519-donna/curve25519_VALVE_sse2.c
@@ -2,7 +2,11 @@
    where it is explicitly placed in the public domain. This wrapper compiles it with the _sse2
    function suffix to be distinguished from the native C version. */
 
+#if defined( _M_IX86 ) || defined( _M_X64 ) || defined(__SSE2__)
+
 #define CURVE25519_SUFFIX _sse2
 #define CURVE25519_SSE2
 
 #include "curve25519.c"
+
+#endif
