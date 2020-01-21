ae735b094a56598f1292f85b6930b9f0abc03687
Steven Noonan <steven@valvesoftware.com>
2020-01-21 00:34:43 -0800

    meson: add -Duse_crypto and -Duse_crypto25519 options
    Signed-off-by: Steven Noonan <steven@valvesoftware.com>

--- .travis/build-meson.sh
+++ .travis/build-meson.sh
@@ -7,7 +7,7 @@ set -e
 
 cleanup() {
 	echo "Cleaning up Meson build directories" >&2
-	rm -rf build-meson
+	rm -rf build-meson build-meson-ref
 }
 
 trap cleanup EXIT
@@ -22,12 +22,15 @@ set -x
 
 # Build lightweight test builds
 meson . build-meson ${MESON_ARGS[@]} --buildtype debugoptimized
+meson . build-meson-ref ${MESON_ARGS[@]} --buildtype debugoptimized -Duse_crypto25519=Reference
 
-# Build all targets of CMake/meson, ensuring everything can build.
+# Build all targets
 ninja -v -C build-meson
+ninja -v -C build-meson-ref
 
 # Run basic tests
 build-meson/tests/test_crypto
+build-meson-ref/tests/test_crypto
 
 set +x
 
