739015003ddeb3f00e6a62bb9e8b086ee3cfe595
Steven Noonan <steven@valvesoftware.com>
2020-05-01 13:24:12 -0700

    steamnetworkingsockets_udp: add missing "override"
    Signed-off-by: Steven Noonan <steven@valvesoftware.com>

--- src/steamnetworkingsockets/clientlib/steamnetworkingsockets_udp.h
+++ src/steamnetworkingsockets/clientlib/steamnetworkingsockets_udp.h
@@ -92,7 +92,7 @@ public:
 	virtual void SendEndToEndConnectRequest( SteamNetworkingMicroseconds usecNow ) override;
 	virtual void SendEndToEndStatsMsg( EStatsReplyRequest eRequest, SteamNetworkingMicroseconds usecNow, const char *pszReason ) override;
 	virtual void TransportConnectionStateChanged( ESteamNetworkingConnectionState eOldState ) override;
-	virtual void TransportPopulateConnectionInfo( SteamNetConnectionInfo_t &info ) const;
+	virtual void TransportPopulateConnectionInfo( SteamNetConnectionInfo_t &info ) const override;
 
 	/// Interface used to talk to the remote host
 	IBoundUDPSocket *m_pSocket;
