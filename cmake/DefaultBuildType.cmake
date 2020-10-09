9a59089e8df10fba46a55cdddbccb5bdf864c8db
Fletcher Dunn <fletcherd@valvesoftware.com>
2020-10-09 11:59:16 -0700

    Move where we dump CMAKE_BUILD_TYPE.
    We need to wait until after we include DefaultBuildType!

--- cmake/DefaultBuildType.cmake
+++ cmake/DefaultBuildType.cmake
@@ -2,9 +2,11 @@
 set(default_build_type "RelWithDebInfo")
 
 if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
-  message(STATUS "Setting build type to '${default_build_type}' as none was specified.")
+  message(STATUS "Setting CMAKE_BUILD_TYPE to '${default_build_type}' as none was specified.")
   set(CMAKE_BUILD_TYPE "${default_build_type}" CACHE STRING "Choose the type of build." FORCE)
 
   # Set the possible values of build type for cmake-gui
   set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
+else()
+  message(STATUS "CMAKE_BUILD_TYPE='${CMAKE_BUILD_TYPE}'")
 endif()
