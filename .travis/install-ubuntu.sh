d3203bb30806d22812415e731b24343495d07fc0
Steven Noonan <steven@valvesoftware.com>
2020-10-27 08:53:05 -0700

    travis/install-ubuntu: cosmetic cleanup
    For consistency with the other install-* scripts.
    
    Signed-off-by: Steven Noonan <steven@valvesoftware.com>

--- .travis/install-ubuntu.sh
+++ .travis/install-ubuntu.sh
@@ -11,7 +11,16 @@ apt-get dist-upgrade -y
 apt-get install -y locales
 locale-gen en_US.UTF-8
 
-PACKAGES=(build-essential pkg-config ccache cmake ninja-build clang git golang)
+PACKAGES=(
+	build-essential
+	pkg-config
+	ccache
+	cmake
+	ninja-build
+	clang
+	git
+	golang
+)
 
 PACKAGES+=(libprotobuf-dev protobuf-compiler)
 PACKAGES+=(libssl-dev)
