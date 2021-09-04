746696b68a0e10c57df8203cdf8c1b6003a23a4f
Fletcher Dunn <fletcherd@valvesoftware.com>
2021-09-04 13:24:07 -0700

    Sync up header with the one in main Steam branch.
    P4:6753549

--- include/steam/steamclientpublic.h
+++ include/steam/steamclientpublic.h
@@ -135,6 +135,8 @@ enum EResult
 	k_EResultLauncherMigrated = 119,			// The specified launcher type is no longer supported; the user should be directed elsewhere
 	k_EResultSteamRealmMismatch = 120,			// The user's realm does not match the realm of the requested resource
 	k_EResultInvalidSignature = 121,			// signature check did not match
+	k_EResultParseFailure = 122,				// Failed to parse input
+	k_EResultNoVerifiedPhone = 123,				// account does not have a verified phone number
 };
 
 // Error codes for use with the voice functions
