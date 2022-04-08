efc004f5a8a43cde2d6ada8b953d19346acf323b
past-due <30942300+past-due@users.noreply.github.com>
2022-04-08 15:15:39 -0400

    Implement CCrypto::GenerateHMAC for libsodium using sha1-wpa implementation

---
+++ src/common/crypto_sha1_wpa.cpp
@@ -0,0 +1,34 @@
+#include "crypto.h"
+#include <tier0/dbg.h>
+#include <tier0/vprof.h>
+
+#ifdef STEAMNETWORKINGSOCKETS_CRYPTO_SHA1_WPA
+
+extern "C" {
+// external headers for sha1 and hmac-sha1 support
+#include "../external/sha1-wpa/sha1.h"
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Generate a keyed-hash MAC using SHA1
+// Input:	pubData -			Plaintext data to digest
+//			cubData -			length of data
+//			pubKey -			key to use in HMAC
+//			cubKey -			length of key
+//			pOutDigest -		Pointer to receive hashed digest output
+//-----------------------------------------------------------------------------
+void CCrypto::GenerateHMAC( const uint8 *pubData, uint32 cubData, const uint8 *pubKey, uint32 cubKey, SHADigest_t *pOutputDigest )
+{
+	VPROF_BUDGET( "CCrypto::GenerateHMAC", VPROF_BUDGETGROUP_ENCRYPTION );
+	Assert( pubData );
+	Assert( cubData > 0 );
+	Assert( pubKey );
+	Assert( cubKey > 0 );
+	Assert( pOutputDigest );
+
+	int status = hmac_sha1(pubKey, cubKey, pubData, cubData, (uint8_t*)pOutputDigest);
+	AssertFatal(status == 0);
+}
+
+#endif // #ifdef STEAMNETWORKINGSOCKETS_CRYPTO_SHA1_WPA
+
---
+++ src/external/sha1-wpa/sha1.h
@@ -0,0 +1,41 @@
+/*
+ * SHA1 hash implementation and interface functions
+ * Copyright (c) 2003-2009, Jouni Malinen <j@w1.fi>
+ *
+ * This software may be distributed under the terms of the BSD license.
+ * See README for more details.
+ */
+
+#ifndef SHA1_H
+#define SHA1_H
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#define os_memcpy	memcpy
+#define os_memset	memset
+
+#define SHA1_MAC_LEN 20
+
+typedef uint8_t u8;
+typedef uint32_t u32;
+
+
+int hmac_sha1_vector(const u8 *key, size_t key_len, size_t num_elem,
+		     const u8 *addr[], const size_t *len, u8 *mac);
+int hmac_sha1(const u8 *key, size_t key_len, const u8 *data, size_t data_len,
+	       u8 *mac);
+
+/**
+ * sha1_vector - SHA-1 hash for data vector
+ * @num_elem: Number of elements in the data vector
+ * @addr: Pointers to the data areas
+ * @len: Lengths of the data blocks
+ * @mac: Buffer for the hash
+ * Returns: 0 on success, -1 on failure
+ */
+int sha1_vector(size_t num_elem, const u8 *addr[], const size_t *len,
+		u8 *mac);
+
+#endif /* SHA1_H */
