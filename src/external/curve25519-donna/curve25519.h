c26b044fb3ad5bde6cca1bb73fe81b24473ba4ac
Fletcher Dunn <fletcherd@valvesoftware.com>
2022-06-08 10:43:27 -0700

    Add extern "C"
    P4:7307202

--- src/external/curve25519-donna/curve25519.h
+++ src/external/curve25519-donna/curve25519.h
@@ -3,8 +3,16 @@
 
 typedef unsigned char curve25519_key[32];
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void curve25519_donna(curve25519_key mypublic, const curve25519_key secret, const curve25519_key basepoint);
 void curve25519_donna_basepoint(curve25519_key mypublic, const curve25519_key secret);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* CURVE25519_H */
 
--- src/external/ed25519-donna/ed25519.h
+++ src/external/ed25519-donna/ed25519.h
@@ -9,6 +9,10 @@ typedef unsigned char ed25519_secret_key[32];
 
 typedef unsigned char curved25519_key[32];
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void ed25519_publickey(const ed25519_secret_key sk, ed25519_public_key pk);
 int ed25519_sign_open(const unsigned char *m, size_t mlen, const ed25519_public_key pk, const ed25519_signature RS);
 void ed25519_sign(const unsigned char *m, size_t mlen, const ed25519_secret_key sk, const ed25519_public_key pk, ed25519_signature RS);
@@ -19,5 +23,8 @@ void ed25519_randombytes_unsafe(void *out, size_t count);
 
 void curved25519_scalarmult_basepoint(curved25519_key pk, const curved25519_key e);
 
+#ifdef __cplusplus
+}
+#endif
 
 #endif // ED25519_H
